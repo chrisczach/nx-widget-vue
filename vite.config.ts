cd970273cfb642c1b00ac8f9f139a28df337a11b
Chris Czach <czach@networkoptix.com>
2022-01-26 19:36:11 -0800

    update instance

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const cloudInstance = 'https://cloud-test.hdw.mx'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       '^^(?!\/widget|\/edit|\/src|\/@vite|\/@id|\/node_modules).{2,}$': {
-        target: 'https://localhost:9000/',
+        target: cloudInstance,
         changeOrigin: true,
         secure: false,
         rewrite: path => `${path}?devServer=https://localhost:3000/widget`
