78be2ad688534a5259c517508568d37ae3cd762a
Chris Czach <czach@networkoptix.com>
2022-01-26 20:23:07 -0800

    remove routes from config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export const cloudInstance = 'https://cloud-test.hdw.mx'
-export const dashboardPath = `${cloudInstance}/dashboard?devServer=https://localhost:3000/widget`
-export const authorizePath = `${cloudInstance}/authorize?redirect_url=${encodeURIComponent(dashboardPath)}`
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
