44984c5c23b0feee2bad9db551b356bebca488eb
Chris Czach <czach@networkoptix.com>
2022-02-10 12:49:09 -0800

    add dynamic instance

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
+const instanceArg = process.argv.reverse()[0]
+
 export const devServer = 'http://127.0.0.1:3000'
-export const cloudInstance = 'https://127.0.0.1:9000'
+export const cloudInstance = instanceArg === '--' ? 'https://127.0.0.1:9000' : instanceArg
