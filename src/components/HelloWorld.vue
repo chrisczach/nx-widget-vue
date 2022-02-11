6acf88fbe9b3ca108b84c2c7d2269d24249223fc
Chris Czach <czach@networkoptix.com>
2022-02-10 18:54:24 -0800

    Update to show systems

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,64 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
-import { SharedWidgetState } from 'dashboard-widget-state'
-import { cloudInstance, devServer } from '../config'
+import { onBeforeMount, ref } from "vue";
+import { SharedWidgetState, System } from "dashboard-widget-state";
+import { cloudInstance, devServer } from "../config";
 
-const dashboardPath = `${cloudInstance}/dashboard?devServer=${devServer}`
+const dashboardPath = `${cloudInstance}/dashboard?devServer=${devServer}`;
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const isShownAsWidget = window.location !== window.parent.location
-const count = ref(0)
-const sharedState = ref<SharedWidgetState>()
+const isShownAsWidget = window.location !== window.parent.location;
+const count = ref(0);
+const navigating = ref("");
+const systems = ref<System[]>([]);
+const sharedState = ref<SharedWidgetState>();
 
 // @ts-ignore
-const isEditMode = editMode
+const isEditMode = editMode;
 
-onBeforeMount(() => new Promise<void>((resolve) => {
-  const stateChecker = setInterval(() => {
-    const stateObject = (window as any).sharedState as SharedWidgetState
-    if (stateObject) {
-      sharedState.value = stateObject;
-      clearInterval(stateChecker)
-      stateObject.state$.subscribe((val: number) => {
-        count.value = val
-      })
-      resolve()
-    }
-  }, 10)
-}))
-
+onBeforeMount(
+  () =>
+    new Promise<void>((resolve) => {
+      const stateChecker = setInterval(() => {
+        const stateObject = (window as any).sharedState as SharedWidgetState;
+        if (stateObject) {
+          sharedState.value = stateObject;
+          clearInterval(stateChecker);
+          stateObject.state$.subscribe((val: number) => {
+            count.value = val;
+          });
+          stateObject.systems$.subscribe((val: System[]) => {
+            systems.value = val;
+          });
+          resolve();
+        }
+      }, 10);
+    })
+);
 </script>
 
 <template>
   <div v-if="isShownAsWidget">
-    <h1>{{ msg }}</h1>
-    <h2 v-if="isEditMode">Edit Mode</h2>
-    <h2 v-else>Widget Mode</h2>
-    <button type="button" @click="sharedState?.increment()">Increment</button>
-    <button type="button" @click="sharedState?.decrement()">Decrement</button>
-    <div>Shared state from cloud portal: {{ count }}</div>
+    <div v-if="navigating">{{ navigating }}</div>
+    <div v-else>
+      <h1>{{ msg }}</h1>
+      <h2 v-if="isEditMode">Edit Mode</h2>
+      <h2 v-else>Widget Mode</h2>
+      <button type="button" @click="sharedState?.increment()">Increment</button>
+      <button type="button" @click="sharedState?.decrement()">Decrement</button>
+      <div>Shared state from cloud portal: {{ count }}</div>
+      <h3>Systems from cloud portal</h3>
+      <section
+        v-for="system in systems"
+        :key="system.id"
+        @click="
+          sharedState?.navigateByUrl(`/systems/${system.id}`);
+          navigating = `Navigating to ${system.name} system`;
+        "
+      >
+        {{ system.name }}
+      </section>
+    </div>
   </div>
   <div v-else>
     <h1 v-if="isEditMode">Edit Mode running</h1>
@@ -51,6 +72,11 @@ a {
   color: #42b983;
 }
 
+section {
+  padding: 16px 8px;
+  border: 1px solid blue;
+  margin-bottom: 8px;
+}
 
 label {
   margin: 0 0.5em;
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
-const instanceArg = process.argv.reverse()[0]
-
 export const devServer = 'http://127.0.0.1:3000'
-export const cloudInstance = instanceArg === '--' ? 'https://127.0.0.1:9000' : instanceArg
+export const cloudInstance = 'https://127.0.0.1:9000'
